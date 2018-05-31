1d1fdc390302372b3a1e8effc8cf2d230bc066a6
dj <deusanyjunior@gmail.com>
2018-05-31 17:37:44 -0300

    update plots
    add heating and time steps!

--- dataGetNPlot.py
+++ dataGetNPlot.py
@@ -18,23 +18,23 @@ gainIdx = 3
 
 
 def calc(v):
-  d1 = 0.
-  d2 = 1.
-  t = 0
+    d1 = 0.
+    d2 = 1.
+    t = 0
 
-  t = v - v0
+    t = v - v0;
 
-  for i in range(0, 4):
-    d1 += t * p[i]
-    t = t * (v - v0)
+    for a in range(0, 4):
+        d1 += t * p[a]
+        t = t * (v - v0);
 
-  t = v - v0
+    t = v - v0;
 
-  for i in range(0, 3):
-    d2 += t * q[i]
-    t = t * (v - v0)
+    for b in range(0, 3):
+        d2 += t * q[b]
+        t = t * (v - v0);
 
-  return (t0 + d1/d2)
+    return (t0 + d1/d2);
 
 #today = datetime.datetime.now()
 
@@ -42,64 +42,95 @@ def calc(v):
 
 sensor1 = [ ]
 sensor2 = [ ]
+
 humidity = [ ]
 lux = [ ]
 ldr = [ ]
-
 dht = [ ]
 
-i = time.time() - 3600 * (12)
+heating = [ ]
+roundy = [ ]
+
+i = time.time() - 3600 * (3)
+time_step = 32
 #end = time.time() - 1000 * 2 - 3600 * ( 12 - 6 )
 
-today = datetime.datetime.fromtimestamp(i)
-t = today.strftime('%Y-%m-%dT%H:%M:%S')
+
+initial = datetime.datetime.fromtimestamp(i)
+t = initial.strftime('%Y-%m-%dT%H:%M:%S')
 
 # t = datetime.datetime.fromtimestamp('2018-03-06T09:30:30')
 labels = []
 
 while True:
 
-	#if len(dht) >= 1000:
-	#	break
+    #if len(dht) >= 1000:
+    #	break
 
-	print t
-
-	json = requests.post(
-		'http://143.107.45.126:30134/collector/resources/' + uuid + '/data',
-		json={'start_date': t, 'end_date': '2018-03-06T16:30:00' })
+    print t
+    #
     # json = requests.post(
-	# 	'http://143.107.45.126:30134/collector/resources/' + uuid + '/data',
-	# 	json={'start_date': t })
+    # 'http://143.107.45.126:30134/collector/resources/' + uuid + '/data',
+    # json={'start_date': t, 'end_date': '2018-06-06T16:30:00' })
 
-	#print 'curl -H "Content-Type: application/json" -X POST -d \'{"start_date":"%s", "end_date": "%s"}\' http://143.107.45.126:30134/collector/resources/e8dcf7f8-4977-4295-a955-3ee3daf18714/data' % (t, endTime)
+    json = requests.post(
+    'http://143.107.45.126:30134/collector/resources/' + uuid + '/data',
+    json={'start_date': t, 'limit': '1000' })
+    # json = requests.post(
+    # 	'http://143.107.45.126:30134/collector/resources/' + uuid + '/data',
+    # 	json={'start_date': t })
 
-	print len(json.json()['resources'][0]['capabilities']['room_monitoring'])
-	print json.text
+    #print 'curl -H "Content-Type: application/json" -X POST -d \'{"start_date":"%s", "end_date": "%s"}\' http://143.107.45.126:30134/collector/resources/e8dcf7f8-4977-4295-a955-3ee3daf18714/data' % (t, endTime)
 
-	if len(json.json()['resources'][0]['capabilities']['room_monitoring']) < 10:
-		break
+    #
+    # if len(json.json()['resources'][0]['capabilities']['room_monitoring']) < 1000:
+    #     break
+    try:
+        data = json.json()['resources'][0]['capabilities']['room_monitoring']
+    except:
+        print "len(data) == 0"
+        break
 
-	data = json.json()['resources'][0]['capabilities']['room_monitoring']
+    print len(data)
+    print "data: "
+    # print data
 
-	for d in data[1:]:
-		sensor1.append(calc(d['ads_0_1'] * scales[gainIdx]) + d['temperature'])
-		sensor2.append(calc(d['ads_2_3'] * scales[gainIdx]) + d['temperature'])
-		dht.append(d['temperature'])
-		ldr.append(d['ldr'])
-		lux.append(d['lux'])
-		humidity.append(d['humidity'])
-		t = d['date']
+    # if len(data) < 1000:
+    #     breakrange(0, len(dht), 4000)
 
-		datetime_object = datetime.datetime.strptime(t[:len(t)-1], '%Y-%m-%dT%H:%M:%S.%f') - datetime.timedelta(hours=3)
 
-		labels.append(datetime_object.strftime('%H:%M:%S'))
+    if t == data[-1]['date']:
+        break;
 
-	i += 1000 * 2
+    for d in data[1:]:
+        try:
+            sensor1.append(calc(d['ads_0_1'] * scales[gainIdx]) + d['temperature'])
+            sensor2.append(calc(d['ads_2_3'] * scales[gainIdx]) + d['temperature'])
+            dht.append(d['temperature'])
+            ldr.append(d['ldr'])
+            lux.append(d['lux'])
+            humidity.append(d['humidity'])
+            t = d['date']
+            heating.append(d['heating']+24)
+            roundy.append(d['round'])
 
-plt.ylim(20, 45)
+            datetime_object = datetime.datetime.strptime(t[:len(t)-1], '%Y-%m-%dT%H:%M:%S.%f') - datetime.timedelta(hours=3)
 
-plt.xticks(range(0, len(dht), 4000), [ labels[i] for i in range(0, len(dht), 4000)], rotation = 30)
+            labels.append(datetime_object.strftime('%H:%M:%S'))
+        except KeyError, e:
+            print "KeyError: " + str(e)
+            print d
+            datetime_object = datetime.datetime.strptime(t[:len(t)-1], '%Y-%m-%dT%H:%M:%S.%f') - datetime.timedelta(hours=3)
+            labels.append(datetime_object.strftime('%H:%M:%S'))
+            continue;
+            # break;
 
+    i += 1000 * 2
+
+plt.ylim(15, 30)
+
+plt.xticks(range(0, len(dht), time_step), [ labels[j] for j in range(0, len(dht), time_step)], rotation = 30)
+#plt.xticks(range(0,len(dht)),labels,rotation='vertical')
 #ax = plt.add_subplot(111)
 
 #ax.set_xlabel('xlabel')
@@ -109,16 +140,18 @@ plt.ylabel('Temperature (Celsius)')
 
 sensor1_line, = plt.plot(sensor1)
 DHT_line, = plt.plot(dht)
+heating_line, = plt.plot(heating)
+# roundy_line, = plt.plot(roundy)
 
-plt.legend([sensor1_line, DHT_line], ['Sensor 1', 'DHT'])
+plt.legend([sensor1_line, DHT_line, heating_line], ['Sensor 1', 'DHT','heating'])
 
 ###
 
 plt.figure()
 
-plt.ylim(20, 45)
+plt.ylim(15, 30)
 
-plt.xticks(range(0, len(dht), 4000), [ labels[i] for i in range(0, len(dht), 4000)], rotation = 30)
+plt.xticks(range(0, len(dht), time_step), [ labels[j] for j in range(0, len(dht), time_step)], rotation = 30)
 
 #ax = plt.add_subplot(111)
 
@@ -129,16 +162,17 @@ plt.ylabel('Temperature (Celsius)')
 
 sensor2_line, = plt.plot(sensor2)
 DHT_line, = plt.plot(dht)
+heating_line, = plt.plot(heating)
 
-plt.legend([sensor2_line, DHT_line], ['Sensor 2', 'DHT'])
+plt.legend([sensor2_line, DHT_line, heating_line], ['Sensor 2', 'DHT','heating'])
 
 #####
 
 plt.figure()
 
-plt.ylim(0, 100)
+plt.ylim(40, 100)
 
-plt.xticks(range(0, len(dht), 4000), [ labels[i] for i in range(0, len(dht), 4000)], rotation = 30)
+plt.xticks(range(0, len(dht), time_step), [ labels[j] for j in range(0, len(dht), time_step)], rotation = 30)
 
 #ax = plt.add_subplot(111)
 
@@ -156,7 +190,7 @@ plt.legend([humidity_line], ['Humidity'])
 
 # plt.figure()
 #
-# plt.xticks(range(0, len(dht), 4000), [ labels[i] for i in range(0, len(dht), 4000)], rotation = 30)
+# plt.xticks(range(0, len(dht), time_step), [ labels[j] for j in range(0, len(dht), time_step)], rotation = 30)
 #
 # #ax = plt.add_subplot(111)
 #
@@ -171,22 +205,22 @@ plt.legend([humidity_line], ['Humidity'])
 
 ##############
 
-plt.figure()
-
-plt.ylim(950, 1024)
-
-plt.xticks(range(0, len(dht), 4000), [ labels[i] for i in range(0, len(dht), 4000)], rotation = 30)
-
-#ax = plt.add_subplot(111)
-
-#ax.set_xlabel('xlabel')
-plt.title('LDR')
-#plt.ylabel('%')
-
-
-ldr_line, = plt.plot(ldr)
-
-plt.legend([ldr_line], ['LDR'])
+# plt.figure()
+#
+# plt.ylim(950, 1024)
+#
+# plt.xticks(range(0, len(dht), time_step), [ labels[j] for j in range(0, len(dht), time_step)], rotation = 30)
+#
+# #ax = plt.add_subplot(111)
+#
+# #ax.set_xlabel('xlabel')
+# plt.title('LDR')
+# #plt.ylabel('%')
+#
+#
+# ldr_line, = plt.plot(ldr)
+#
+# plt.legend([ldr_line], ['LDR'])
 
 #####
 
