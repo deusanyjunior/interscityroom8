fa953a2f8a2b62c790262e876ff74ac09d6afee6
dj <deusanyjunior@gmail.com>
2018-07-16 16:51:53 -0300

    atualizando o monitor da porta serial

--- monitorPySerial.py
+++ monitorPySerial.py
@@ -4,9 +4,9 @@ from datetime import datetime
 import requests
 import json
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 outputfile = open("sensorsRoom8-"+datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')+".csv","w");
-outputfile.write("sensorLDR,sensorLux,sensorDHT22Humidity,sensorDHT22TemperatureCelsius,sensorADS_0_1,sensorADS_2_3")
+outputfile.write("sensorLDR,sensorSoilMoisture,sensorDHT22Humidity,sensorDHT22TemperatureCelsius,sensorADS_0_1,sensorADS_2_3")
 
 # uuid = 'f6a72f8c-dc5f-42b9-bc25-cd7c6a66d1c9' ## uuid for testing
 uuid = 'e8dcf7f8-4977-4295-a955-3ee3daf18714'
@@ -25,17 +25,19 @@ while True:
         {{
             "data": [
                 {{
-                    "timestamp"  : "{0}",
-                    "round"      : "{1}",
-                    "ldr"        : "{2}",
-                    "lux"        : "{3}",
-                    "humidity"   : "{4}",
-                    "temperature": "{5}",
-                    "ads_0_1"    : "{6}",
-                    "ads_2_3"    : "{7}",
-                    "adsm_0_1"   : "{8}",
-                    "adsm_2_3"   : "{9}",
-                    "heating"    : "{10}"
+                    "timestamp"		: "{0}",
+                    "round"		: "{1}",
+                    "ldr"		: "{2}",
+                    "soil_moisture"	: "{3}",
+                    "humidity"   	: "{4}",
+                    "temperature"	: "{5}",
+                    "ads_0_1"    	: "{6}",
+                    "ads_2_3"    	: "{7}",
+                    "adsm_0_1"   	: "{8}",
+                    "adsm_2_3"   	: "{9}",
+                    "ads2_0_1"		: "{10}",
+                    "ads2_2_3"		: "{11}",
+                    "heating"    	: "{12}"
                 }}
             ]
         }}
@@ -48,4 +50,4 @@ while True:
 	r = requests.post('http://143.107.45.126:30134/adaptor/resources/' + uuid + '/data/room_monitoring', json=json_data)
 	print (len(r.text))
 
-	outputfile.write(timestamp+","+data[0]+","+data[1]+","+data[2]+","+data[3]+","+data[4]+","+data[5]+","+str(len(r.text))+"\n");
+	outputfile.write(timestamp+","+','.join(str(e) for e in data)+","+str(len(r.text))+"\n");
