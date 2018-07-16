73d5261a509f8ccbe778aa81ab135a20d8d96b3e
dj <deusanyjunior@gmail.com>
2018-07-16 17:33:03 -0300

    adicionando variável do ganho

--- interscityroom8.ino
+++ interscityroom8.ino
@@ -27,6 +27,7 @@ float hum = 0;  //Stores humidity value
 float temp = 0; //Stores temperature value
 
 // ADS
+adsGain_t selectedAdsGain;
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 Adafruit_ADS1115 ads2(0x49);  /* Use this for the 16-bit version */
 //int16_t adc0, adc1, adc2, adc3;
@@ -35,103 +36,7 @@ int16_t adc2_0_1, adc2_2_3;
 
 const float voltage_to_temp = 0.039;
 const float ads_to_voltage  = 0.0078125;
-float magical = ads_to_voltage / voltage_to_temp;/*
-2
- Based on:
-3
-   Arduino example AnalogOutSerial.ino
-4
-   Lux sensor: https://github.com/claws/BH1750
-5
-   DHT 22: http://www.ardumotive.com/how-to-use-dht-22-sensor-en.html
-6
-   ADS1115: http://henrysbench.capnfatz.com/henrys-bench/arduino-voltage-measurements/arduino-ads1115-module-getting-started-tutorial/
-7
-*/
-8
-​
-9
-#include <Wire.h>
-10
-#include <BH1750.h>
-11
-#include <dht.h>
-12
-#include <Adafruit_ADS1015.h>
-13
-​
-14
-​
-15
-// LDR
-16
-const int analogInPin = A6;  // Analog input pin that the LDR is attached to
-17
-int sensorValueLDR = 0;        // value read from the LDR
-18
-​
-19
-// Lux
-20
-// BH1750 lightMeter;
-21
-uint16_t lux = 0;
-22
-​
-23
-// DHT 22
-24
-dht DHT;
-25
-#define DHT22_PIN 4
-26
-float hum = 0;  //Stores humidity value
-27
-float temp = 0; //Stores temperature value
-28
-​
-29
-// ADS
-30
-Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-31
-​
-32
-void setup() {
-33
-  // initialize serial communications at 9600 bps:
-34
-  Serial.begin(9600);
-35
- 
-36
- 
-37
- // Lux
-38
-  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
-39
-  // On esp8266 devices you can select SCL and SDA pins using Wire.begin(D4, D3);
-40
-  // Wire.begin();
-41
-​
-42
-  //lightMeter.begin();
-43
-  
-44
-// ADS
-45
-  // The ADC input range (or gain) can be changed via the following
-46
-  // functions, but be careful never to exceed VDD +0.3V max, or to
-47
-  // exceed the upper and lower limits if you adjust the input range!
-48
-  // Setting these values incorrectly may destroy your ADC!
-49
-  //                                                                ADS1015  ADS1115
+float magical = ads_to_voltage / voltage_to_temp;
 float adcm_0_1, adcm_2_3;
 
 // Print separator
@@ -167,16 +72,24 @@ void setup() {
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
   // Setting these values incorrectlanalogIny may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-   ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-//  ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
-  ads2.setGain(GAIN_FOUR);
+  //                                                                ADS1015  ADS1115            value
+  //                                                                -------  -------            -----
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default) 0
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV            512
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV           1024
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV          1536
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV         2048
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV        2560
+  
+//  selectedAdsGain = GAIN_TWOTHIRDS;
+//  selectedAdsGain = GAIN_ONE;
+//  selectedAdsGain = GAIN_TWO;
+//  selectedAdsGain = GAIN_FOUR;
+  selectedAdsGain = GAIN_EIGHT;
+//  selectedAdsGain = GAIN_SIXTEEN;
+  
+  ads.setGain(selectedAdsGain);
+  ads2.setGain(selectedAdsGain);
   
   ads.begin();
   ads2.begin();
@@ -231,7 +144,8 @@ void loop() {
   adc2_2_3 = ads2.readADC_Differential_2_3();
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Prints
+  // OUTPUT 
+  
   // debug only
   if (!__DEBUG__) {
     if (counter == 0) digitalWrite(2, HIGH);
@@ -259,7 +173,7 @@ void loop() {
     //Serial.print("sensorDHT22TemperatureCelsius,");
     Serial.print(temp);
     Serial.print(sep);
-    Serial.print("\t");
+//    Serial.print("\t");
    
 // ADS
   // single ended
@@ -288,6 +202,9 @@ void loop() {
   Serial.print(adc2_2_3);
   Serial.print(sep);
 
+// ADS GAIN
+  Serial.print(selectedAdsGain);
+  Serial.print(sep);
 
 // Heating
   Serial.print(digitalRead(2));
